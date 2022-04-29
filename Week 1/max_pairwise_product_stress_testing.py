847d7352e8910e7447c85e06e9c6c45a19449dd9
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-04-29 16:06:24 -0600

    Update max_pairwise_product_stress_testing.py

--- Week 1/max_pairwise_product_stress_testing.py
+++ Week 1/max_pairwise_product_stress_testing.py
@@ -24,6 +24,8 @@ def max_pairwise_product_fast(number_list): #a faster algorithm, requires n comp
     for i in range(2,n): #compare biggest to remaining eleements
         if number_list[i] >= biggest:
             second_biggest, biggest = biggest, number_list[i]
+        elif number_list[i] > second_biggest:
+            second_biggest = number_list[i]
     return biggest * second_biggest
 
 def stress_test(N, M):
