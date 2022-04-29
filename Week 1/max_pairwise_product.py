5c587924f3846f35dd7fdc72f8ed3d4851c20c4e
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-04-29 12:01:17 -0600

    Update max_pairwise_product.py

--- Week 1/max_pairwise_product.py
+++ Week 1/max_pairwise_product.py
@@ -1,19 +1,11 @@
-def biggest_two_numbers(number_list):
-    n = len(number_list)
+def max_pairwise_product(number_list):
     biggest = max(number_list[0], number_list[1])
     second_biggest = min(number_list[0], number_list[1])
     for number in number_list:
-        print(number, biggest, second_biggest)
         if biggest < number:
             second_biggest = biggest
             biggest = number
-    return biggest, second_biggest
-
-def max_pairwise_product(numbers):
-    first, second = biggest_two_numbers(numbers)
-    n = len(numbers)
-    max_product = first * second
-    return max_product
+    return biggest * second_biggest
 
 if __name__ == '__main__':
     input_n = int(input())
