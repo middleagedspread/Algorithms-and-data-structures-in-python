d1efee694e7867663a79fae920edf77f7505c780
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-04-29 17:14:28 -0600

    Created separate algorithm files for different pairwise product strategies

--- Week 1/max_pairwise_product.py
+++ Week 1/max_pairwise_product.py
@@ -13,11 +13,11 @@ def max_pairwise_product_fast(number_list): #a faster algorithm, requires n comp
         biggest, second_biggest = number_list[1],  number_list[0]
     
     for i in range(2,n): #compare biggest to remaining elements
-        if number_list[i] >= biggest:
-            second_biggest, biggest = biggest, number_list[i]
-        elif number_list[i] > second_biggest:
-            second_biggest = number_list[i]
-    return biggest * second_biggest
+        if number_list[i] >= biggest: # if the next number is bigger than biggest
+            second_biggest, biggest = biggest, number_list[i] #update both biggest and second biggest
+        elif number_list[i] > second_biggest: # if it is only bigger than second biggest
+            second_biggest = number_list[i] # update second_biggest
+    return biggest * second_biggest # return the product of the two biggest numbers
 
 
 if __name__ == '__main__':
